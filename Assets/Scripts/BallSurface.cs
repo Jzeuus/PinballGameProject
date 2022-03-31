6ef7cb4b82158f929963be13d95b0c01da840983
Jesus Alvarado <inkakola9@gmail.com>
2022-03-31 13:18:35 -0400

    first commit

---
+++ Assets/Scripts/BallSurface.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSurface : MonoBehaviour
+{
+    public GameObject floor;
+    public GameObject pinBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0.1f, 0, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position.Set(0,
+            floor.transform.position.y + 0.17501f
+            , 0);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bumper")){
+            //right now everytime the ball touches the bumper, the score will be update by 10
+            //later could change the value to random
+            GameManager.Instance.score += 10;
+            //update the score
+
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //extra credit for, custom made obstacles, sketchup is for free, import custom made obstacle
+
+    public GameObject floor;
+    public GameObject pinBall;
+    public int score = 0;
+    public static GameManager Instance;
+    public GameState gameState;
+    public static event Action<GameState> OnGameStateChanged;
+    public TMP_Text scoreboard;
+
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateGameState(GameState.Idle);
+
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+
+
+        switch (newState)
+        {
+            case GameState.Idle:
+                gameState = GameState.Idle;
+                break;
+            case GameState.Playing:
+                
+                break;
+            case GameState.WinLevel:
+                
+
+                break;
+            case GameState.LoseLevel:
+                break;
+            case GameState.WinGame:
+                break;
+            case GameState.GameOver:
+                break;
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+
+
+    public void FixedUpdate()
+    {
+        //set text UI for collected points
+        scoreboard.SetText("score: " + score);
+
+
+
+    }
+
+    public enum GameState
+    {
+        Idle,
+        Playing,
+        WinLevel,
+        LoseLevel,
+        WinGame,
+        GameOver
+    }
+
+}
